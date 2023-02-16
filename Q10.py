163b06739a72a84e8b9068567bf17ede5b54b486
Harshwardhan <Harshwardhanw.tagline@gmail.com>
2023-02-16 12:36:55 +0530

    Change Made, Explaining of complex added using comment

--- Q10.py
+++ Q10.py
@@ -1,16 +1,26 @@
 def diamond_pattern(n):
+
+    #i have iterated over loop to print the pattern, i have print _ in odd-number and * in even number occurence.
     for i in range(1, n + 1, 2):
         print('* ' * int((n - i) / 2) + '- ' * i + '* ' * int((n - i) / 2))
+
+    #i have iterated over loop to print the pattern, i have print * in odd-number and _ in even number occurence.
     for i in range(n - 2, 0, -2):
         print('* ' * int((n - i) / 2) + '_ ' * i + '* ' * int((n - i) / 2))
 
 def diamond_mid_pattern(n):
+
+    #i have iterated over loop to print the pattern, i have print * in odd-number and _ in even number occurence.
     for i in range(1, n + 1, 2):
         print('_ ' * int((n - i) / 2) + '* ' * i + '_ ' * int((n - i) / 2))
+    
+    #i have iterated over loop to print the pattern, i have print _ in odd-number and * in even number occurence.
     for i in range(n - 2, 0, -2):
         print('_ ' * int((n - i) / 2) + '* ' * i + '_ ' * int((n - i) / 2))
 
 def triangle_pattern(n):
+
+    #i have iterated over loop to print the pattern, but over here i have print _ in the center of triangle using condition.
     for i in range(0, n+1):
         for j in range(0, i):
             if (i==3 and j==1) or (i==4 and (j==1 or j==2)):
@@ -20,6 +30,8 @@ def triangle_pattern(n):
         print("\r")
 
 def square_pattern(n):
+
+    #i have iterated over loop to print the pattern, but over here i have print _ in the center of square using condition.
     for i in range(1,n+1):
         for j in range(1, n+1):
             if i==1 or j==1 or i==5 or j==5:
@@ -29,6 +41,8 @@ def square_pattern(n):
         print("\r")
 
 def number_triangle_pattern(n):
+
+    #i have iterated over loop to print the number pattern, num variable is increases in every iteration and printed 
     num = 1
     for i in range(0, n+1):
         for j in range(0,i):
