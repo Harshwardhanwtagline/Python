c0b2a05d9e96f3c2142f24c5dc60df77fdebbe18
Harshwardhan <Harshwardhanw.tagline@gmail.com>
2023-02-16 12:11:28 +0530

    Change Made, Explaining of complex added using comment

--- Q8.py
+++ Q8.py
@@ -1,11 +1,12 @@
-def list_sort(list_1):
-    for i in range(0, len(list_1) - 1):
-        for j in range(0, len(list_1) - i - 1):
-            if list_1[j] > list_1[j + 1]:
-                list_1[j], list_1[j + 1] = list_1[j + 1], list_1[j]
-    return list_1
+def list_sort(num_list):
+    #using bubble sort algorithm i have sorted the list
+    for i in range(0, len(num_list) - 1):
+        for j in range(0, len(num_list) - i - 1):
+            if num_list[j] > num_list[j + 1]:
+                num_list[j], num_list[j + 1] = num_list[j + 1], num_list[j]
+    return num_list
 
 
 if __name__ == "__main__":
-    list_1 = [2, 5, 6, 1, 3, 8, 9, 10]
-    print("Sorted List is : {}".format(list_sort(list_1)))
+    num_list = [2, 5, 6, 1, 3, 8, 9, 10]
+    print("Sorted List is : {}".format(list_sort(num_list)))
