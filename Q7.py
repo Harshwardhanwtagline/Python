9797ed1c008b84474c35de1ace58469549cf5826
Harshwardhan <Harshwardhanw.tagline@gmail.com>
2023-02-16 12:09:10 +0530

    Change Made, Explaining of complex added using comment

--- Q7.py
+++ Q7.py
@@ -1,7 +1,9 @@
 import re
 
 def valid_email(emails):
+    #I have make pattern variable, on bases of given condition using regex. 
     pattern = r'\b[A-Za-z0-9._-]+@[A-Za-z0-9]+\.[A-Za-z]{2,3}\b'
+    #I have loop over the emails_list, use match and remove function to remove the unsupport mail form list.   
     for i in emails[:]:
         if not bool(re.match(pattern, i)):
             emails.remove(i)
