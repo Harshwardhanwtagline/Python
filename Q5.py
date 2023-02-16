80325d369e9e5c02e4592c95c981a52a4c06de63
Harshwardhan <Harshwardhanw.tagline@gmail.com>
2023-02-16 11:47:35 +0530

    Change Made, Explaining of complex added using comment

--- Q5.py
+++ Q5.py
@@ -2,8 +2,11 @@ from itertools import permutations
 
 
 def list_sum(num_list, user_input):
+    #I have use itertools-library to get permutations of different element 
     permutations_list = list(permutations(num_list, 2))
+    #i have separated the combination of list , who's sum is equal to user_input and convert into list
     sum_list = [list(_) for _ in set(permutations_list) if sum(_) == user_input]
+    #after then i have filter the dulipcate on base of condition.
     result = list(filter(lambda x: x[0]< x[1] , sum_list))
     return result
 
