11ec03c83b65c0122c28141d568b3d8b8b0f0f5a
Harshwardhan <Harshwardhanw.tagline@gmail.com>
2023-02-17 09:51:22 +0530

    Name convention of variable is change

--- Q11.py
+++ Q11.py
@@ -1,7 +1,7 @@
 def correct_ap(a_p):
     # First i have find the difference b/w the element
-    diff_list = [a_p[_+1]-a_p[_] for _ in range(0, len(a_p)-1)]
-    diff_count_dict = {_:diff_list.count(_) for _ in diff_list}
+    diff_list = [a_p[ele+1]-a_p[ele] for ele in range(0, len(a_p)-1)]
+    diff_count_dict = {diff_ele:diff_list.count(diff_ele) for diff_ele in diff_list}
     difference_value = max(zip(diff_count_dict.values(), diff_count_dict.keys()))[1]
 
     #separating the element
@@ -21,14 +21,14 @@ def correct_ap(a_p):
 
 def correct_gp(g_p):
     # First i have to find the difference b/w the element
-    diff_list = [g_p[_+1]/g_p[_] for _ in range(0, len(g_p)-1)]
-    diff_count_dict = {_:diff_list.count(_) for _ in diff_list}
-    difference_value = max(zip(diff_count_dict.values(), diff_count_dict.keys()))[1]
+    ratio_list = [g_p[ele+1]/g_p[ele] for ele in range(0, len(g_p)-1)]
+    ratio_count_dict = {ratio_ele:ratio_list.count(ratio_ele) for ratio_ele in ratio_list}
+    ratio_value = max(zip(ratio_count_dict.values(), ratio_count_dict.keys()))[1]
 
     #separating the element
     g_p_element = []
     for i in range(0, len(g_p)-1):
-        if (g_p[i+1]/g_p[i]) == difference_value:
+        if (g_p[i+1]/g_p[i]) == ratio_value:
             g_p_element.append(g_p[i+1]), g_p_element.append(g_p[i])
         else:
             g_p_element.append(g_p[i])
@@ -37,7 +37,7 @@ def correct_gp(g_p):
     # Replacing the Wrong Element with Correct Element
     max_num_index = g_p.index(max(g_p_element))
     if len(set(g_p_element)) != len(g_p):
-        g_p[max_num_index+1] = round(g_p[max_num_index]*difference_value)
+        g_p[max_num_index+1] = round(g_p[max_num_index]*ratio_value)
     return g_p
 
 
