b8d1a4d2af32fa8c59df6fc1a2d4eb40ba36c860
Harshwardhan <Harshwardhanw.tagline@gmail.com>
2023-02-17 09:35:13 +0530

    Change Made, Dynamic code made

--- Q10.py
+++ Q10.py
@@ -2,42 +2,43 @@ def diamond_pattern(n):
 
     #i have iterated over loop to print the pattern, i have print _ in odd-number and * in even number occurence.
     for i in range(1, n + 1, 2):
-        print('* ' * int((n - i) / 2) + '- ' * i + '* ' * int((n - i) / 2))
+        print('*' * int((n - i) / 2) + '-' * i + '*' * int((n - i) / 2))
 
     #i have iterated over loop to print the pattern, i have print * in odd-number and _ in even number occurence.
     for i in range(n - 2, 0, -2):
-        print('* ' * int((n - i) / 2) + '_ ' * i + '* ' * int((n - i) / 2))
+        print('*' * int((n - i) / 2) + '_' * i + '*' * int((n - i) / 2))
 
 def diamond_mid_pattern(n):
 
     #i have iterated over loop to print the pattern, i have print * in odd-number and _ in even number occurence.
     for i in range(1, n + 1, 2):
-        print('_ ' * int((n - i) / 2) + '* ' * i + '_ ' * int((n - i) / 2))
+        print('_' * int((n - i) / 2) + '*' * i + '_' * int((n - i) / 2))
     
     #i have iterated over loop to print the pattern, i have print _ in odd-number and * in even number occurence.
     for i in range(n - 2, 0, -2):
-        print('_ ' * int((n - i) / 2) + '* ' * i + '_ ' * int((n - i) / 2))
+        print('_' * int((n - i) / 2) + '*' * i + '_' * int((n - i) / 2))
 
 def triangle_pattern(n):
 
     #i have iterated over loop to print the pattern, but over here i have print _ in the center of triangle using condition.
-    for i in range(0, n+1):
-        for j in range(0, i):
-            if (i==3 and j==1) or (i==4 and (j==1 or j==2)):
-                print("_", end=" ")
+    for i in range(1, n+1):
+        for j in range(1, i + 1):
+            if j == 1 or j == i or i == 5:
+                print("*", end="")
             else:
-                print("*", end=" ")
+                print("_", end="")
         print("\r")
+
 
 def square_pattern(n):
 
     #i have iterated over loop to print the pattern, but over here i have print _ in the center of square using condition.
-    for i in range(1,n+1):
+    for i in range(0,n+1):
         for j in range(1, n+1):
-            if i==1 or j==1 or i==5 or j==5:
-                print("*", end=" ")
-            else:
+            if i==1 or j==1 or i==n or j==n:
                 print("_", end=" ")
+            else:
+                print("*", end=" ")
         print("\r")
 
 def number_triangle_pattern(n):
@@ -52,9 +53,11 @@ def number_triangle_pattern(n):
 
 
 if __name__ == "__main__":
-    n = 5
-    diamond_pattern(n)
-    diamond_mid_pattern(n)
+    n = 6
+    # diamond_pattern(n)
+    # diamond_mid_pattern(n)
     triangle_pattern(n)
-    square_pattern(n)
-    number_triangle_pattern(n)
+    # square_pattern(n)
+    # number_triangle_pattern(n)
+
+    
