b20e1b8144b4b3cc9399e464a977cd1d7757f1c9
Harshwardhan <Harshwardhanw.tagline@gmail.com>
2023-02-17 18:27:37 +0530

    Q9 Completed with Portion Library.

--- Q9.py
+++ Q9.py
@@ -1,25 +1,41 @@
-from datetime import datetime, timedelta
+import portion as P
+from datetime import datetime, timedelta, time
+
 
 def difference(start_time, end_time):
+    # I have take starting time to its 12:00:00 AM because we needed in separation of night time for particular day.
+    start_date_am = datetime.combine(start_time.date(), datetime.min.time())
+    night_interval_dt = []
+    # i have iterated over loop of date and capture in interval of night time by using timedelta.
+    while start_date_am <= end_time:
+        night_interval_dt.append(
+            P.closed(
+                datetime.combine(start_date_am, time()),
+                datetime.combine(start_date_am, time()) + timedelta(hours=6),
+            )
+        )
+        start_date_am += timedelta(days=1)
+    # Here i have intersection night_time from total_time interval.
+    interval_night = [
+        P.closed(start_time, end_time).intersection(night_time)
+        for night_time in night_interval_dt
+    ]
+    # i have calculate total time in hours
+    total_time = (end_time - start_time).total_seconds() / 3600
+    # Here i have calculate total_night_time in hours
+    total_night_time = sum(
+        [(i.upper - i.lower).total_seconds() / 3600 for i in interval_night]
+    )
+    return total_time - total_night_time
 
-    #In below code i have loop over start_time and end_time, after then i have separated the night_interval on base of 1 hr interval using timedelta
-    night_interval_hr = []
-    while end_time > start_time:
-        if datetime(start_time.year, start_time.month, start_time.day,0,0,0) < start_time < datetime(start_time.year, start_time.month, start_time.day,6,0,0):
-            night_interval_hr.append((start_time, start_time + timedelta(hours=1)))
-        start_time += timedelta(hours=1)
-    
-    #i have calculate the total of night_interval_hr and remove it from total_hr by subtracing the start_time and end_time.
-    night_hr = [(date[-1]-date[0]).total_seconds() for date in night_interval_hr]
-    total_night_hr = sum(night_hr)/3600
-    total_time_seconds = (end_time - start_time).total_seconds()
-    total_time_hours = total_time_seconds / 3600
-    return total_time_hours-total_night_hr
 
 if __name__ == "__main__":
-    start_time =  datetime.strptime("2022-10-18 02:00:00", "%Y-%m-%d %H:%M:%S")
-    end_time =  datetime.strptime("2022-10-18 08:00:00", "%Y-%m-%d %H:%M:%S")
-
-    print(difference(start_time, end_time))
-
-
+    user_input_start_time = str(input("Enter the Start Time: "))
+    user_input_end_time = str(input("Enter the End Time: "))
+    start_time = datetime.strptime(user_input_start_time, "%Y-%m-%d %H:%M:%S")
+    end_time = datetime.strptime(user_input_end_time, "%Y-%m-%d %H:%M:%S")
+    print(
+        "The Total Time Difference in Hours is : {}".format(
+            difference(start_time, end_time)
+        )
+    )
