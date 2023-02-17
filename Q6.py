8c2680f856bf4db6cb8967394b7f311082ab2e0c
Harshwardhan <Harshwardhanw.tagline@gmail.com>
2023-02-17 16:31:39 +0530

    Variable name change , and indentation is corrected

--- Q6.py
+++ Q6.py
@@ -1,10 +1,14 @@
 def count_substring(string, substring):
-
-    #Using list comprehension and slicing , i have count the occcurence of substring in string. and then i have append 1 in list,
-    #after the i have sum all using sum function
-    count = sum([1 for _ in range(0,len(string)) if string[_:len(substring)+_] == substring])
+    # Using list comprehension and slicing, i have count the occcurence of substring in string. and then i have append 1 in list,
+    # after the i have sum all using sum function
+    count = sum(
+        [
+            1
+            for index in range(0, len(string))
+            if string[index : len(substring) + index] == substring
+        ]
+    )
     return count
-
 
 if __name__ == "__main__":
     string = "PQRQRQRQ"
